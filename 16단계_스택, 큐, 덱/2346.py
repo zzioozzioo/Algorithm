3cdabda31377c0a02aed082e4a286f0b40bf0283
zzioozzioo <zzioo1006@naver.com>
2024-06-05 20:47:56 +0900

    commit

--- 16단계_스택, 큐, 덱/2346.py
+++ 16단계_스택, 큐, 덱/2346.py
@@ -1,16 +1,50 @@
 import sys
 from collections import deque
 
-input = sys.stdin.readline
+n = int(sys.stdin.readline())
 
-N = int(sys.stdin.readline().rstrip())
-deq = deque(map(int, sys.stdin.readline().rstrip().split()))
+deq = deque(enumerate(map(int, sys.stdin.readline().split()), start=1))
+for i in range(n):
+	p = deq.popleft() # pop(0)과 같은 결과를 가져오지만, pop(0)보다 더 빠르다
+	print(p[0], end=' ')
+	if p[1] > 0:
+		deq.rotate(-(p[1] - 1)) # popleft된 1칸을 제외하고 시계 반대방향으로 회전
+	else:
+		deq.rotate(-p[1]) # 시계 방향으로 회전
 
-index = 0
-for i in range(N):
-    index = index % len(deq)
-    index = deq.pop(deq[index])
-    if index > 0:
-        index = deq.pop(deq[index-1])
-    else:
-        index = deq.pop(deq[index])
+# # 다른 풀이 1 - enumerate(), list() 사용 - 시간복잡도 O(N)
+# import sys
+# input = sys.stdin.readline
+
+# N = int(sys.stdin.readline().rstrip())
+# data = list(enumerate(map(int, sys.stdin.readline().rstrip().split()), start=1))
+
+# index = 0 # 첫 번째 index 고정
+# while data:
+#     value = data.pop(index)
+#     print(value[0], end=' ')
+#     if value[1] < 0 and data: # 다음 인덱스 위치가 음수이고 리스트에 원소가 남아있으면
+#         index = (index+value[1]) % len(data)
+#     elif value[1] > 0 and data:
+#         index = (index+value[1]-1) % len(data)
+
+# # 다른 풀이 2 - enumerate(), deque(), rotate() 사용
+# import sys
+# from collections import deque
+
+# input = sys.stdin.readline()
+
+# N = int(sys.stdin.readline().rstrip())
+# deq = deque(enumerate(map(int, sys.stdin.readline().rstrip().split())))
+# result = []
+
+# while deq:
+#     index, paper_num = deq.popleft()
+#     result.append(index+1)
+
+#     if paper_num > 0:
+#         deq.rotate(-(paper_num-1))
+#     else:
+#         deq.rotate(-paper_num)
+
+# print(' '.join(map(str, result)))
---
+++ 21단계_재귀/25501.py
@@ -0,0 +1,23 @@
+import sys
+
+input = sys.stdin.readline
+
+def recursion(s, l, r):
+    global count
+    count += 1
+
+    if l >= r:
+        return 1
+    elif s[l] != s[r]:
+        return 0
+    else:
+        return recursion(s, l+1, r-1)
+    
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
+
+T = int(input().rstrip())
+for i in range(T):
+    S = input().rstrip()
+    count = 0
+    print(isPalindrome(S), count)
