5b39a9487d9f23e0fbeec8959db510da83bc2136
zzioozzioo <zzioo1006@naver.com>
2024-08-10 23:09:13 +0900

    프로그래머스 깊이/너비 우선 탐색 네트워크

--- JAVA/DFS:BFS/타겟 넘버.java
+++ JAVA/DFS:BFS/타겟 넘버.java
@@ -1,22 +1,24 @@
-import java.util.ArrayList;
-
 class Solution {
-    
+
     int answer = 0;
 
     public int solution(int[] numbers, int target) {
-        // DFS
-        dfs(numbers, 0, target, 0);
+        int sum = 0;
+        int depth = 0;
+
+        DFS(numbers, depth, target, sum);
 
         return answer;
     }
 
-    public void dfs(int[] numbers, int depth, int target, int sum) {
+    public void DFS(int[] numbers, int depth, int target, int sum) {
         if(depth == numbers.length) { // 마지막 노드까지 탐색한 경우
-            if(target == sum) answer++;
+            if(target == sum) {
+                answer++;
+            }
         } else {
-            dfs(numbers, depth+1, target, sum+numbers[depth]);
-            dfs(numbers, depth+1, target, sum-numbers[depth]);
+            DFS(numbers, depth+1, target, sum+numbers[depth]);
+            DFS(numbers, depth+1, target, sum-numbers[depth]);
         }
     }
 }
