cb63307d4345e991399f66550c54c2e94b103a45
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2024-11-18 21:58:05 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -0,0 +1,15 @@
+x, y, w, h = map(int, input().split())
+x_dis = 0
+y_dis = 0
+
+if x > w/2:
+    x_dis = w-x
+else:
+    x_dis = x
+
+if y > h/2:
+    y_dis = h-y
+else:
+    y_dis = y
+
+print(min(x_dis, y_dis))
