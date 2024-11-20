adfe6c9d3d64bfb199991a79bfd0063cc9a6d430
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2024-11-20 21:51:03 +0900

    [Bronze III] Title: 네 번째 점, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.py
@@ -0,0 +1,21 @@
+a, b = map(int, input().split())
+c, d = map(int, input().split())
+e, f = map(int, input().split())
+
+x, y = 0, 0
+
+if a==c:
+    x = e
+elif a==e:
+    x = c
+else:
+    x = a
+
+if b==d:
+    y = f
+elif b==f:
+    y = d
+else:
+    y = b
+
+print(x, y)
