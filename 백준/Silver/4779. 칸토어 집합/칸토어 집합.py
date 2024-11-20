195ba33f6e443656f5d9f4ebb62e890ec88333f5
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2024-11-20 21:51:49 +0900

    [Silver III] Title: 칸토어 집합, Time: 92 ms, Memory: 36652 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -0,0 +1,23 @@
+def Cantor(start, len):
+    # line.remove(line[(3**N)*(1/3):(3**N)*(2/3)+1:]) # 가운데 구간 삭제 
+
+    if len == 1:
+        return
+    for i in range(start+len//3, start+(len//3)*2):
+        line[i] = ' ' # 가운데 문자열(1/3~2/3 구간)을 공백으로 변경
+    Cantor(start, len//3)
+    Cantor(start+len//3*2, len//3)
+
+import sys
+
+input = sys.stdin.readline
+
+while True:
+    try:
+        N = int(input().rstrip())
+        line = ['-']*(3**N) # 최초 리스트
+        Cantor(0, 3**N)
+        print(''.join(line))   
+    except :
+        break
+
