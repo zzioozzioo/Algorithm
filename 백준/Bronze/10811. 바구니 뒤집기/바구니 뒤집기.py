63fdbfcd676bb26d993a00d91aff1d19b414c7d7
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2024-11-21 23:55:10 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.py
@@ -0,0 +1,11 @@
+n, m = map(int, input().split())
+list = list(range(1, n+1))
+
+for i in range(m):
+    i, j = map(int, input().split())
+    temp = list[i-1:j]
+    temp.reverse()
+    list[i-1:j]= temp
+
+for i in range(n):
+    print(list[i], end=' ')
