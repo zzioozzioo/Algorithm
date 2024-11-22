aa9514702b923fdcb4d7a94623e42e3cf8e6cf23
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2024-11-22 21:08:59 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+def fibo(x):
+    if x==0:
+        return 0
+    elif x==1:
+        return 1
+    else:
+        return fibo(x-1)+fibo(x-2)
+
+n = int(input().rstrip())
+
+print(fibo(n))
