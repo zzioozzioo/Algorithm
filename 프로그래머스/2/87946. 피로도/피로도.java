b8628ce9b11a5399ea5bf1acf898d33c4de3deb9
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2024-11-28 14:37:58 +0900

    [level 2] Title: 피로도, Time: 4.34 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,25 @@
+class Solution {
+
+    public int answer; // 던전 최대 탐험 수
+    public boolean[] visited; // 던전 방문 여부
+
+    public int solution(int k, int[][] dungeons) {
+        
+        visited = new boolean[dungeons.length];
+
+        dfs(0, k, dungeons);
+
+        return answer;
+    }
+
+    public void dfs(int depth, int k, int[][] dungeons) {
+        for(int i=0; i<dungeons.length; i++) {
+            if(!visited[i] && dungeons[i][0] <= k) {
+                visited[i] = true;
+                dfs(depth+1, k-dungeons[i][1], dungeons);
+                visited[i] = false;
+            }
+        }
+        answer = Math.max(answer, depth);
+    }
+}
