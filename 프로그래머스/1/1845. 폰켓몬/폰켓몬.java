c2fe0d38ab20e9abdcc748274c68e3b98850d776
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2025-01-10 22:58:38 +0900

    [level 1] Title: 폰켓몬, Time: 6.32 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+
+public class Solution {
+    public int solution(int[] nums) {
+        
+        int max = nums.length / 2; // 폰켓몬 최대로 선택 가능한 수
+
+        // 중복 제거하기 위한 set
+        HashSet<Integer> numsSet = new HashSet<>();
+
+        for (int num:nums) { // numsSet 해시에 nums의 요소 추가
+            numsSet.add(num);
+        }
+
+        if (numsSet.size() > max) {
+            return max;
+        } else {
+            return numsSet.size();
+        }
+
+    }
+}
