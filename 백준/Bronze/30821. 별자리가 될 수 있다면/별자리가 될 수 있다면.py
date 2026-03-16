ea2aceb7615a52c85da9a1f3e1dc6b65e3318c56
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2026-03-16 11:23:10 +0900

    [Bronze II] Title: 별자리가 될 수 있다면, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/30821. 별자리가 될 수 있다면/별자리가 될 수 있다면.py
+++ 백준/Bronze/30821. 별자리가 될 수 있다면/별자리가 될 수 있다면.py
@@ -1,14 +1,14 @@
 def factorial(num):
     # 재귀로 구현
-    if num==0 or num==1:
-        return 1
-    return num*factorial(num-1)
+    # if num==0 or num==1:
+    #     return 1
+    # return num*factorial(num-1)
 
     # 반복문으로 구현
-    # result = 1
-    # for i in range(2, num+1):
-    #     result *= i
-    # return result
+    result = 1
+    for i in range(2, num+1):
+        result *= i
+    return result
 
 N = int(input())
 
