30c0605f1e33474605c7995acbf55dc6d7388610
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2026-03-19 14:04:57 +0900

    [Bronze III] Title: 약수 구하기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.py
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -1,11 +1,14 @@
 N, K = map(int, input().split())
-list = []
 
+count = 0
 for i in range(1, N+1):
-    if N % i == 0:
-        list.append(i)
-
-if len(list) < K:
-    print('0')
+    if N%i == 0:
+        count += 1
+    
+    if count == K:
+        print(i)
+        break
 else:
-    print(list[K-1])
+    if count < K:
+        print(0)
+
