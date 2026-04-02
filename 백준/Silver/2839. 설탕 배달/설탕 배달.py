fdbb196157f0f90b2434b71484bf1c516fa425ec
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2026-04-02 14:18:19 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,22 +1,16 @@
 N = int(input())
-num_list = []
-# five, three = 0, 0
+num_bag = 0
 
-# five = N//5
-# three = (N-five*5)//3 # 혹은 N%5도 가능
-# if five*5+three*3 != N:
-#     if N%3 == 0:
-#         three = N//3
-#         print(three)
-#     else:        
-#         print(-1)
-# else:
-#     print(five+three)
-for i in range(N):
-    for j in range(N):
-        if 5*i+3*j == N:
-            num_list.append(i+j)
-if len(num_list) != 0:
-    print(min(num_list))
-else:
-    print(-1)
+while True:
+    if N%5==0:
+        num_bag += N//5
+        break
+    
+    if N<3:
+        num_bag = -1
+        break
+    
+    N -= 3
+    num_bag += 1
+
+print(num_bag)
