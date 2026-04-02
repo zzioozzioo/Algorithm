2d9c1c3bcedc4e4bdd58863a0cd3911e5d62655a
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2026-04-02 13:59:44 +0900

    [Bronze II] Title: 블랙잭, Time: 52 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,15 +1,14 @@
 N, M = map(int, input().split())
-num_list = sorted(list(map(int, input().split())))
-n_list = []
-sum = 0
+card_list = [int(card) for card in input().split()]
 
-for i in range(N-2): # N-1인 이유: k의 최대가 N이니까 j의 최대는 N-1, i의 최대는 N-2이 되어야 최대가 됐을 때 수들이 겹치지 않는다. 
-    for j in range(i+1, N-1):
-        for k in range(j+1, N):
-            sum = num_list[i] + num_list[j] + num_list[k]
-            if sum > M:
-                continue
-            else:
-                n_list.append(sum)
+def findMax():
+    max = 0
+    for card1_index in range(N-2):
+        for card2_index in range(card1_index+1, N-1):
+            for card3_index in range(card2_index+1, N):
+                sum = card_list[card1_index] + card_list[card2_index] + card_list[card3_index]
+                if sum == M: return sum
+                elif max < sum < M: max = sum
+    return max
 
-print(max(n_list))
+print(findMax())                
