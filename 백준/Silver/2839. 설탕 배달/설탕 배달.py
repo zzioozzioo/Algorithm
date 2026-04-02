841400446a800f8999b5aef3b8984b5bd74ec3b9
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2026-04-02 13:33:05 +0900

    [Silver IV] Title: 설탕 배달, Time: 3544 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,23 +1,22 @@
 N = int(input())
+num_list = []
+# five, three = 0, 0
 
-# 자료구조 정의
-# 5kg, 3kg의 최대 봉지 수
-max_5kg = N//5
-max_3kg = N//3
-min_bag = max_5kg + max_3kg
-
-is_found = False
-
-# 알고리즘
-for num_5kg in range(0, max_5kg+1):
-    for num_3kg in range(0, max_3kg+1):
-        if num_5kg*5 + num_3kg*3 == N:
-            is_found = True
-            cur_bag = num_3kg + num_5kg
-            if min_bag > cur_bag:
-                min_bag = cur_bag
-
-if is_found:
-    print(min_bag)
+# five = N//5
+# three = (N-five*5)//3 # 혹은 N%5도 가능
+# if five*5+three*3 != N:
+#     if N%3 == 0:
+#         three = N//3
+#         print(three)
+#     else:        
+#         print(-1)
+# else:
+#     print(five+three)
+for i in range(N):
+    for j in range(N):
+        if 5*i+3*j == N:
+            num_list.append(i+j)
+if len(num_list) != 0:
+    print(min(num_list))
 else:
     print(-1)
