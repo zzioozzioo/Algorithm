d350f30439b74c9e5111d52d6a9d2a28ded1447f
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2026-04-08 09:46:53 +0900

    [Bronze II] Title: 분해합, Time: 896 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -2,9 +2,7 @@ N = int(input())
 num_list = []
 sum_i = 0
 for i in range(1, N+1):
-    # str_i = str(i)
-    # for j in range(len(str_i)):
-    #     sum += int(str_i[j])
+    
     sum_i = sum(map(int, str(i)))
     if N == sum_i + i:
         num_list.append(i)
