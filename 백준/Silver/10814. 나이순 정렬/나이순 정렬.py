d8a42445b8b85ed9c1497c17ebd5b7d08117e957
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2026-04-09 11:12:39 +0900

    [Silver V] Title: 나이순 정렬, Time: 204 ms, Memory: 54212 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -1,14 +1,12 @@
 import sys
+input = sys.stdin.readline
 
-N = int(sys.stdin.readline())
-member_list = []
-
+N = int(input())
+members = []
 for _ in range(N):
-    [age, name] = list(sys.stdin.readline().split())
-    age = int(age)
-    member_list.append([age, name])
+    members.append(input().split())
 
-member_list.sort(key=lambda x: (x[0]))
+members.sort(key=lambda x: int(x[0]))
 
-for i in member_list:
-    print(i[0], i[1])
+for member in members:
+    print(' '.join(member))
