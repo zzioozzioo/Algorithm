3e47de2965c941500c5154ba77f65027b3f21772
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2026-04-16 09:03:53 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 7676 ms, Memory: 51528 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.py
@@ -1,22 +1,17 @@
-import sys
-
-input = sys.stdin.readline
 N, M = map(int, input().split())
 
-int_key = {} # key값이 int인 dictionary
-name_key = {} # key값이 str인 dictionary
+int_key = {} 
+name_key = {}
 
-# 포켓몬 저장
 for i in range(1, N+1):
     data = input().strip()
-    int_key[i] = data # key: 번호, value: 포켓몬 이름
-    name_key[data] = i # key: 포켓몬 이름, value: 번호
+    int_key[i] = data
+    name_key[data] = i
 
-# 포켓몬 탐색
 for _ in range(M):
     data = input().strip()
 
-    if data.isdigit() == True: # isdigit(): 숫자로만 이루어져 있는지 판별, 맞으면 True
+    if data.isdigit() == True:
         print(int_key[int(data)])
     else:
         print(name_key[data])
