14cde42ede314dfad5a95cda7da414e693d9f475
zzioozzioo <165160185+zzioozzioo@users.noreply.github.com>
2026-08-01 23:55:03 +0900

    [level 0] Title: 수 조작하기 1, Time: 7.62 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,12 @@
+def solution(n, control):
+    
+    for i in range(len(control)):
+        if control[i] == 'w':
+            n += 1
+        elif control[i] == 's':
+            n -= 1
+        elif control[i] == 'd':
+            n += 10
+        else:
+            n -= 10
+    return n
